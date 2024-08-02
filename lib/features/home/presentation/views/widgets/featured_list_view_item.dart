8ae9745892e19a8c3c57a63dc5f05911ab28c8fc
mohammedashraf16 <mohammedashraf1692003@gmail.com>
2024-08-02 19:26:26 +0300

    Add featured list view to home

---
+++ lib/features/home/presentation/views/widgets/featured_list_view_item.dart
@@ -0,0 +1,22 @@
+import 'package:bookely_app/core/utils/assets.dart';
+import 'package:flutter/material.dart';
+
+class FeaturedListViewItem extends StatelessWidget {
+  const FeaturedListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 2.7/4,
+      child: Container(
+        decoration:  BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          image: const DecorationImage(
+              image: AssetImage(AssetsData.testImage),
+              fit: BoxFit.fill
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,5 +1,5 @@
 import 'package:bookely_app/features/home/presentation/views/widgets/custom_app_bar.dart';
-import 'package:bookely_app/features/home/presentation/views/widgets/custom_list_view_item.dart';
+import 'package:bookely_app/features/home/presentation/views/widgets/featured_list_view_item.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -11,9 +11,29 @@ class HomeViewBody extends StatelessWidget {
     return const Column(
       children: [
         CustomAppBar(),
-        CustomListViewItem()
+        FeaturedBooksListView(),
       ],
     );
   }
 }
 
+class FeaturedBooksListView extends StatelessWidget {
+  const FeaturedBooksListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * .3,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: 5,
+        itemBuilder: (context, index) {
+          return const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8.0),
+            child: FeaturedListViewItem(),
+          );
+        },
+      ),
+    );
+  }
+}
