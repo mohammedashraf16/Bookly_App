213edc961bdcabdc87f61622c8ac12658b368b8f
mohammedashraf16 <mohammedashraf1692003@gmail.com>
2024-08-02 17:52:16 +0300

    Adding Sliding Animation to Splash View.

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,18 +1,55 @@
 import 'package:bookely_app/core/utils/assets.dart';
+import 'package:bookely_app/features/splash/presentation/views/widgets/sliding_text.dart';
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
 
   @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 5), end: Offset.zero)
+            .animate(animationController);
+    animationController.forward();
+    slidingAnimation.addListener(
+      () {
+        setState(() {});
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    animationController.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return  Column(
+    return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        Image.asset(AssetsData.logo),
+        SlideTransition(
+          position: slidingAnimation,
+          child: Image.asset(AssetsData.logo),
+        ),
         const SizedBox(height: 5),
-        const Text("Read Free Books",textAlign: TextAlign.center,)
+        SlidingText(slidingAnimation: slidingAnimation)
       ],
     );
   }
