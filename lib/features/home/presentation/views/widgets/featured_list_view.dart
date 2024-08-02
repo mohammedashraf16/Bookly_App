f36e4ce86c917a1d85d21ce3cd73d90f9975b90f
mohammedashraf16 <mohammedashraf1692003@gmail.com>
2024-08-02 19:39:11 +0300

    Create styles file and add title to home view.

---
+++ lib/features/home/presentation/views/widgets/featured_list_view.dart
@@ -0,0 +1,23 @@
+import 'package:bookely_app/features/home/presentation/views/widgets/featured_list_view_item.dart';
+import 'package:flutter/material.dart';
+
+class FeaturedBooksListView extends StatelessWidget {
+  const FeaturedBooksListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * .3,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: 5,
+        itemBuilder: (context, index) {
+          return const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8.0),
+            child: FeaturedListViewItem(),
+          );
+        },
+      ),
+    );
+  }
+}
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,5 +1,6 @@
+import 'package:bookely_app/core/utils/styles.dart';
 import 'package:bookely_app/features/home/presentation/views/widgets/custom_app_bar.dart';
-import 'package:bookely_app/features/home/presentation/views/widgets/featured_list_view_item.dart';
+import 'package:bookely_app/features/home/presentation/views/widgets/featured_list_view.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -8,31 +9,19 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [
-        CustomAppBar(),
-        FeaturedBooksListView(),
-      ],
-    );
-  }
-}
-
-class FeaturedBooksListView extends StatelessWidget {
-  const FeaturedBooksListView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * .3,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        itemCount: 5,
-        itemBuilder: (context, index) {
-          return const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 8.0),
-            child: FeaturedListViewItem(),
-          );
-        },
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 24.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CustomAppBar(),
+          FeaturedBooksListView(),
+          SizedBox(height: 50),
+          Text(
+            "Best Seller",
+            style: AppStyles.titleMedium,
+          )
+        ],
       ),
     );
   }
