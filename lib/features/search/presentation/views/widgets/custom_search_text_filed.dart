4a57ec5d1e837a8e41d87f8f8db47d1bea09226c
mohammedashraf16 <mohammedashraf1692003@gmail.com>
2024-08-03 21:31:47 +0300

    create custom search text filed.

---
+++ lib/features/search/presentation/views/widgets/custom_search_text_filed.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class CustomSearchTextFiled extends StatelessWidget {
+  const CustomSearchTextFiled({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(
+        enabledBorder: buildOutlineInputBorder(),
+        focusedBorder: buildOutlineInputBorder(),
+        hintText: "Search",
+        suffixIcon: IconButton(
+          onPressed: () {},
+          icon: const Opacity(
+            opacity: .7,
+            child: Icon(
+              FontAwesomeIcons.magnifyingGlass,
+              size: 22,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  OutlineInputBorder buildOutlineInputBorder() {
+    return OutlineInputBorder(
+      borderSide: const BorderSide(
+        color: Colors.white,
+      ),
+      borderRadius: BorderRadius.circular(12),
+    );
+  }
+}
