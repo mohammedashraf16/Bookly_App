1ddf7ab0438a6cede45c7bc44ac56f4e51079ac1
mohammedashraf16 <mohammedashraf1692003@gmail.com>
2024-08-03 13:28:51 +0300

    add custom book item to book details view

--- lib/features/home/presentation/views/widgets/featured_list_view.dart
+++ lib/features/home/presentation/views/widgets/featured_list_view.dart
@@ -1,4 +1,4 @@
-import 'package:bookely_app/features/home/presentation/views/widgets/featured_list_view_item.dart';
+import 'package:bookely_app/features/home/presentation/views/widgets/custom_book_item.dart';
 import 'package:flutter/material.dart';
 
 class FeaturedBooksListView extends StatelessWidget {
@@ -14,7 +14,7 @@ class FeaturedBooksListView extends StatelessWidget {
         itemBuilder: (context, index) {
           return const Padding(
             padding: EdgeInsets.symmetric(horizontal: 8.0),
-            child: FeaturedListViewItem(),
+            child: CustomBookImage(),
           );
         },
       ),
