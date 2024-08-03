74e96cfa478c875c55941d1ff56894a78827f0fb
mohammedashraf16 <mohammedashraf1692003@gmail.com>
2024-08-03 22:18:30 +0300

    add book entity.

---
+++ lib/features/home/domain/book_entity.dart
@@ -0,0 +1,10 @@
+class BookEntity{
+  final String image;
+
+  BookEntity(this.image, this.title, this.authorName, this.price, this.rating);
+
+  final String title;
+  final String authorName;
+  final num price;
+  final num rating;
+}
