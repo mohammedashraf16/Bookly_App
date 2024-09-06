4061e45cbb6ad8c76c17134f845eca81db75e21a
mohammedashraf16 <mohammedashraf1692003@gmail.com>
2024-09-07 00:14:18 +0300

    refactor to use case class

--- lib/features/home/domain/use_cases/fetch_featured_books_use_case.dart
+++ lib/features/home/domain/use_cases/fetch_featured_books_use_case.dart
@@ -1,12 +1,19 @@
 import 'package:bookely_app/core/errors/failure.dart';
 import 'package:bookely_app/features/home/domain/entities/book_entity.dart';
 import 'package:bookely_app/features/home/domain/repos/home_repo.dart';
+import 'package:bookely_app/core/use_cases/use_case.dart';
 import 'package:dartz/dartz.dart';
 
-class FetchFeaturedBooksUseCase{
+class FetchFeaturedBooksUseCase extends UseCase<List<BookEntity>, NoParam> {
   final HomeRepo homeRepo;
+
   FetchFeaturedBooksUseCase(this.homeRepo);
-  Future<Either <Failure,List<BookEntity>>> fetchFeaturedBooks(){
-    return homeRepo.fetchFeaturedBooks();
+
+  @override
+  Future<Either<Failure, List<BookEntity>>> call([NoParam? param]) async {
+    return await homeRepo.fetchFeaturedBooks();
   }
 }
+
+
+
