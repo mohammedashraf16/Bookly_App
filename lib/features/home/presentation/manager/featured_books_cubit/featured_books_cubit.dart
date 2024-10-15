35d582dcd2969e7f1482694898e573552071cd27
mohammedashraf16 <mohammedashraf1692003@gmail.com>
2024-10-15 21:56:58 +0300

    create featured books cubit

--- lib/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart
+++ lib/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart
@@ -1,7 +1,19 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookely_app/features/home/domain/entities/book_entity.dart';
+import 'package:bookely_app/features/home/domain/use_cases/fetch_featured_books_use_case.dart';
 part 'featured_books_state.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+  FetchFeaturedBooksUseCase fetchFeaturedBooksUseCase;
+  FeaturedBooksCubit(this.fetchFeaturedBooksUseCase) : super(FeaturedBooksInitial());
+
+  Future<void> fetchFeaturedBooks()async{
+    emit(FeaturedBooksLoading());
+    var result=await fetchFeaturedBooksUseCase.call();
+    result.fold((failure) {
+      emit(FeaturedBooksFailure(failure.message));
+    }, (books) {
+      emit(FeaturedBooksSuccess(books));
+    },);
+  }
 }
