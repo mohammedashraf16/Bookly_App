9b1cc316c82cb3498ef3c97bd91b9d679b799ff9
mohammedashraf16 <mohammedashraf1692003@gmail.com>
2024-10-15 21:45:07 +0300

    create featured books states & bloc observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:bloc/bloc.dart';
 import 'package:bookely_app/constants.dart';
 import 'package:bookely_app/core/utils/app_router.dart';
+import 'package:bookely_app/core/utils/observer.dart';
 import 'package:bookely_app/features/home/domain/entities/book_entity.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -8,6 +10,7 @@ import 'package:hive_flutter/hive_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  Bloc.observer = MyBlocObserver();
   await Hive.initFlutter();
   Hive.registerAdapter(BookEntityAdapter());
   await Hive.openBox<BookEntity>(kFeaturedBox);
